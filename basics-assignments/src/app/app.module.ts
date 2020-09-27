fbf474e96a2679eee10ba908fb83efbaba466b31
RadhikaRJ <radhika7002@gmail.com>
2020-09-27 15:53:42 +0530

    Component creation Assignment

---
+++ basics-assignments/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
